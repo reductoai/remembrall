7d213335780e3415671fca5fd8bd07295eee6586
Raunak Chowdhuri <raunak@oloren.ai>
2023-10-09 23:28:21 -0400

    fix

--- src/app/api/trpc/[trpc]/route.ts
+++ src/app/api/trpc/[trpc]/route.ts
@@ -3,7 +3,6 @@ import { env } from "~/env.mjs";
 import { appRouter } from "~/server/api/root";
 import { createTRPCContext } from "~/server/api/trpc";
 
-// hello
 export const maxDuration = 300; // This function can run for a maximum of 300 seconds
 
 const handler = (req: Request) =>
