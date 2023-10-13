8fd4c34243caf4e3e9e8d060d1ee6130cee98e21
Raunak Chowdhuri <raunakc@mit.edu>
2023-10-13 16:16:26 -0400

    add line breaks

--- src/app/(home)/chat.tsx
+++ src/app/(home)/chat.tsx
@@ -70,7 +70,7 @@ export default function HomepageChat({ vanilla }: { vanilla?: boolean }) {
               <div
                 key={index}
                 className={cn(
-                  "flex w-fit max-w-[75%] flex-col gap-2 rounded-lg px-3 py-2 text-sm",
+                  "flex w-fit max-w-[75%] flex-col gap-2 whitespace-pre-wrap rounded-lg px-3 py-2 text-sm",
                   message.role === "user"
                     ? "ml-auto bg-muted"
                     : "bg-primary text-primary-foreground",
